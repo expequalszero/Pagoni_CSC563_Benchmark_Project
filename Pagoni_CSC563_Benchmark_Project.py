40a4a938b46c9a4627fc82fda1f85cbc33c1ecb2
Brian Pagoni <pagonib1@southernct.edu>
2023-09-23 15:21:32 -0400

    added a welcome GUI, giving the user options to choose from. removed some code that was redundent. more enhancements to come.

--- Pagoni_CSC563_Benchmark_Project.py
+++ Pagoni_CSC563_Benchmark_Project.py
@@ -3,18 +3,19 @@
 
 #importing the different modules that will be used to assist 
 
-from asyncio import futures
+
+
 import time
-import threading
+
 import tkinter as tk
 from tkinter import ttk
 import concurrent.futures
 import math
-from tkinter import font
-from turtle import bgcolor
 
 
-operationsCount = 100_000       # for testing purposes 
+
+
+
 
 def getDeviation(data):          #ran into issue with statistics given off the wall returns for standard deviation. using a custom deviation function for time being
     
@@ -31,128 +32,153 @@ def getDeviation(data):          #ran into issue with statistics given off the w
 
 
 
-def getCounts(testType, operation,threadCount): #function for getting the num of operations completed per second, some arguments are needed for return to track 
-    setOpsCount =100_000
-    opsCount = [] 
-    durationCount = []
-    deviation,average = 0,0
-    for _ in range(3):
-        if(testType == "setNumTime"):
-            numOps = 0 
-            startTime = time.perf_counter() #getting the start time using performance counter for ms 
+def getCounts(testType, operation,threadCount):         #function for getting require results, broken into two options. 
+    setOpsCount =100_000_000                            #varible to test for the number of operations, current value is so testing can be done quickly
+    opsCount = []                                       #varible to hold number of operations complated in a 1 second
+    durationCount = []                                  #varible to hold how long it takes each iteration of the operation test
+    for _ in range(3):                                  #running the test three times
+        if(testType == "setNumTime"):                   #checking testType varible, either setNumTime or setNumOps - 
+            numOps = 0                                  #initial value of number of operations completed
+            startTime = time.perf_counter()             #getting the start time using performance counter for ms 
         
-            while time.perf_counter()-startTime <1:   #running a loop for 1 second and getting the number of operatiosn completed.
-                eval(operation)
-                numOps+=1
+            while time.perf_counter()-startTime <1:     #running a loop for 1 second and getting the number of operatiosn completed.
+                eval(operation)                         #the provided operation needs to be evaluated 
+                numOps+=1                               #tracking number of operations complated
         
-            opsCount.append(numOps) #adding the number of operations to list 
-        
-        else:
-            startTime = time.perf_counter()
-            for _ in range(setOpsCount):
-                eval(operation)
-            endTime =  time.perf_counter()
-            duration = endTime-startTime
-            opsPerSec = setOpsCount/duration#playing around with the time 
-            durationCount.append(opsPerSec)
+            opsCount.append(numOps)                     #adding the number of operations to list to use for later calculations 
+        else:                                           #if the testType is other thant setNumTime 
+            startTime = time.perf_counter()             #recording start time 
+            for _ in range(setOpsCount):                #running loop based on the set number of operations
+                eval(operation)                         #evaluating the operation
+            endTime =  time.perf_counter()              #recording the time after all operations are completed
+            duration = endTime-startTime                #getting the total duration of time 
+            opsPerSec = setOpsCount/duration            #divide the total number of operations by the total time to get the time it takes to complete operations per second  
+            durationCount.append(opsPerSec)             #add the results to the list 
             
-    if(testType == "setNumTime"):
+    if(testType == "setNumTime"):                       #based on the test type: send the appropriate list to the function to return the deviation and average 
        deviation,average = getDeviation(opsCount) 
     else:   
        deviation,average = getDeviation(durationCount) 
 
-    return threadCount,average, deviation #returning thread count, operation type, average and deviation 
+    return threadCount,average, deviation               #returning thread count, average and deviation 
 
 
-def threadCountUsed(benchType):
-
-    results= []
-    numThreads = [1,2,4,8]
+def threadCountUsed(benchType,gui):                         #function to use a particular number of threads
+    gui.destroy()                                       #closed the welcome page once option is selected 
+    results= []                                         #list to hold results 
+    numThreads = [1,2,4,8]                              #list to hold thread count
     
    
     results.extend([("float",) + getCounts(benchType,"2.0+1.0","default")])  #getting base for whatever the computer will do on its own.
-    results.extend([("int",) + getCounts(benchType,"2+1","default")])
+    results.extend([("int",) + getCounts(benchType,"2+1","default")])        
     
-    with concurrent.futures.ThreadPoolExecutor() as executor: #getting results based on the number of threads used, using thread pool executor . 
-        futuresF = [executor.submit(getCounts,benchType,"2.0+1.0",threadCount) for threadCount in numThreads]
-        results.extend((("float",) + future.result()) for future in futuresF)
+    with concurrent.futures.ThreadPoolExecutor() as executor:                                                        #getting results based on the number of threads used, using thread pool executor . 
+        futuresF = [executor.submit(getCounts,benchType,"2.0+1.0",threadCount) for threadCount in numThreads]        #using a loop to send elements of numThread to executor module to run function based on thread count 
+        results.extend((("float",) + future.result()) for future in futuresF)                                        #adding results to the list 
         futuresI = [executor.submit(getCounts,benchType,"2+1",threadCount) for threadCount in numThreads]
         results.extend((("int",) + future.result()) for future in futuresI)
-    print( "Completed tasks for set time, results returned to user")
-    return results
+    print( "Completed tasks for set time, results returned to user")                                                 #advise the user on terminal that the operations were completed
+   
+    createGUI(benchType,results)                                                                                                 #return the results to user
 
-
-
-
-
-
-def createGUI(cpuSpeeds,type):
-    #creating the gui 
-    guiDisplay = tk.Tk()
-    #set background color
-    guiDisplay.config(bg="skyblue")
+    
+def createGUI(type, cpuSpeeds):                                                                                      #function to create a GUI 
+                                                                                                                    #creating the gui 
+    guiDisplay = tk.Tk()                                                                                            #set the guis configuration up
+    guiDisplay.config(bg="skyblue")                                                                         
     guiDisplay.grid_rowconfigure(0,weight=1)
     guiDisplay.grid_columnconfigure(0,weight=1)
-
-    guiDisplay.title(f"Benchmark Program: CPU Speeds {type}")
-    #creating a nested list to hold frames for gui
+    if type =="setNumTime":
+        typeHeader = "Using a Set Duration of Time"
+    else:
+        typeHeader = "Using a Set Amount of Operations"
+    guiDisplay.title(f"Benchmark Program: CPU Speeds {typeHeader}")                                                       #creating a nested list to hold frames for gui
     frames = [[None for _ in range(4)] for _ in range(len(cpuSpeeds))]
-    
-    
-    headerStyle = ttk.Style() #setting the style up for the gui options
-    headerStyle.configure("headerStyle.TLabel", font=("Times New Roman",14,"bold"), background = "lightgrey")
+    headerStyle = ttk.Style()                                                                                       #setting the style up for the gui header and body
+    headerStyleName = "headerStyle.TLabel"
+    headerStyle.configure(headerStyleName, font=("Times New Roman",14,"bold"), background = "lightgrey")
     bodyStyle = ttk.Style()
-    bodyStyle.configure("body.TLabel", font =("Times New Roman", 12))
-
-
-    guiHeaders = [ "Operation Type: ","Number of Threads: ", "Average of Operations per second: ", "Standard Deviation: "]
-
-
-
-    for head, headers in enumerate(guiHeaders):
+    bodyStyleName = "body.TLabel"
+    bodyStyle.configure(bodyStyleName, font =("Times New Roman", 12))
+   
+    guiHeaders = [ "Operation Type: ","Number of Threads: ", "Average of Operations per second: ", "Standard Deviation: "] #list of header titles
+    for head, headers in enumerate(guiHeaders):                                                         #filling in the header on gui 
         headerFrame = tk.Frame(guiDisplay, relief="solid", borderwidth=1, bg="lightgrey")
         headerFrame.grid(row=0,column=head,padx =10, pady=10,sticky ="w")
-        
-        label = ttk.Label(headerFrame, text = headers, anchor ='w', style="headerStyle.TLabel")
+        label = ttk.Label(headerFrame, text = headers, anchor ='w', style= headerStyleName)
         label.grid(row=0,column= head, padx = 10, pady =10, sticky ='w')
         
-        
-
-    for row, result in enumerate (cpuSpeeds):
-        print("row " ,row, "result: ", result)
-        
-        
+    for row, result in enumerate (cpuSpeeds):                                                       #running through the results and filling in the GUI - using enumerate to run through the results list 
         for col, data in enumerate(result):
-            print("col: ", col, "data: ", data)
-              
-            dataFrame = tk.Frame(guiDisplay,relief="solid",borderwidth = 1)
+            dataFrame = tk.Frame(guiDisplay,relief="solid",borderwidth = 1)                         #using frames and configuration frames design 
             dataFrame.grid(row= row+1, column = col, padx =10, pady=10)
-            frames[row][col] = dataFrame
-           
-            labelText = ""
-                
-            if col == 0 and data == "float":
+            frames[row][col] = dataFrame                                                            #adding frames to the list based on location 
+             
+            if col == 0 and data == "float":                                                        #seeing what type of operations were conducted and filling in the labels accordingly
                 labelText = "Float Operations Per Second (FLOPS): "
             elif col == 0 and data == "int":
                 labelText = "Integer Operations Per Second (IOPS): "
             else:
                 labelText = str(data)
          
-            label = ttk.Label(dataFrame,text = labelText, anchor = "w", style = "body.TLabel")   
+            label = ttk.Label(dataFrame,text = labelText, anchor = "w", style = bodyStyleName)      #filling in the labels 
             label.grid(row=row,column = col,sticky ="w")
-    guiDisplay.mainloop()
 
-if __name__ == "__main__":
-     #creating varibles to get the results 
-    cpuSpeedsTime = threadCountUsed("setNumTime")
-    cpuSpeedsOps = threadCountUsed("setNumOps")
-    titleTime = "Using a Set Amount of Time."
-    titleOps = "Using a Set Amount of Operations."
-    threadForTime = threading.Thread(target=createGUI, args=(cpuSpeedsTime,titleTime, ))
-    threadForOps = threading.Thread(target=createGUI, args=(cpuSpeedsOps,titleOps, ))
+    ttk.Button(guiDisplay,text= "Do Another Test", command = lambda: welcomeGUI( guiDisplay)).grid(row=len(cpuSpeeds)+1,column = 4, padx =10,pady=10,sticky ="se" )
 
-    threadForTime.start()
-    threadForOps.start()
+    guiDisplay.mainloop()                                                                           #displaying the GUI
 
-    threadForTime.join()
-    threadForOps.join()
+def welcomeGUI(GUI):                                                                                 #display welcome GUI - ran into overcomplex coding with other route i was goign to take
+
+    if GUI:
+        GUI.destroy()                                                                                  #test to ensure only one instance is running
+
+    gui = tk.Tk()                                   
+    gui.config(bg="skyblue",pady=20)    
+    
+    
+    gui.title(f"Benchmark Program: Welcome Page ")  
+    gui.geometry("800x500+10+10")
+
+    frames = []
+    textList = ["Please select the type of test you would like to run to test you CPU: ","Use a set amount of time:","Use a set number of operations:","Close Benchmark Application"]
+    welcomeFont = ("Times New Roman",14,"bold")
+    
+    for i in range(4):
+        frame = tk.Frame(gui,relief="solid",borderwidth=2,bg="lightgrey")                       #making the frames for the GUI
+        
+        frame.grid(row=i,column=0,padx = 0, pady = 20)
+        frame.grid_rowconfigure(i,weight=1)
+        
+        frames.append( frame)
+
+    labelStyle = ttk.Style()
+    labelStyle.configure("Welcome.TLabel", background ="lightgrey")
+
+    buttonStyle = ttk.Style()
+    buttonStyle.configure("ButtonStyle.TButton",                                                      #ran into an issue with the style for the buttons, still cant figure out why background wont cahnge color 
+                          background = "lightgrey", 
+                          focuscolor = "blue", 
+                          bordercolor= "black",
+                          borderwidth = 2)
+
+    buttonStyle.map("ButtonStyle.TButton",                                                          #mapping for change - however it isnt taking effect, need to do more research for better understanding 
+                    background = [("pressed","green"),
+                                  ("active","green")])
+                    
+   
+
+    ttk.Label(frames[0],text= textList[0],style = "Welcome.TLabel",font = welcomeFont).grid(row=0, column = 0, sticky="ew")
+    ttk.Button(frames[1],text = textList[1], style = "ButtonStyle.TButton", command = lambda:threadCountUsed("setNumOps",gui)).grid(row=1,column = 0,  sticky = "ew")
+    ttk.Button(frames[2],text = textList[2], style ="ButtonStyle.TButton", command = lambda:threadCountUsed("setNumTime:",gui)).grid(row=2,column = 0,  sticky = "ew")
+    ttk.Button(frames[3],text = textList[3], style ="ButtonStyle.TButton", command = lambda:gui.destroy()).grid(row=3,column = 0, sticky = "ew")
+    
+  
+        
+    gui.mainloop()
+
+if __name__== "__main__":
+    
+    welcomeGUI(False)
+
+    
